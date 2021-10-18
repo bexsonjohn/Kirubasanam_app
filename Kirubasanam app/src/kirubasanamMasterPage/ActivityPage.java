3c9bc88b44f18817c8f5ec2cb6f401a0d6af593f
Arise4U <Arise4U@DESKTOP-MAGVE0N>
2021-10-18 14:45:45 +0530

    Hey

---
+++ Kirubasanam app/src/kirubasanamMasterPage/ActivityPage.java
@@ -0,0 +1,43 @@
+package kirubasanamMasterPage;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class ActivityPage extends JFrame {
+
+	private JPanel contentPane;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ActivityPage frame = new ActivityPage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ActivityPage() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 600);
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
---
+++ Kirubasanam app/src/kirubasanamMasterPage/ActivityPanel.java
@@ -0,0 +1,18 @@
+package kirubasanamMasterPage;
+
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+
+public class ActivityPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public ActivityPanel() {
+		
+		JSplitPane splitPane = new JSplitPane();
+		add(splitPane);
+
+	}
+
+}
